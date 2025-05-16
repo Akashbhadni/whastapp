7b7037b6db4e801fc3e17c2116a2f4b7d99c5774
Akashbhadni <asbhai14@gmail.com>
2025-05-16 11:07:26 +0530

    name

--- public/client.js
+++ public/client.js
@@ -2,9 +2,9 @@ const socket = io()
 let name;
 let textarea = document.querySelector('#textarea')
 let messageArea = document.querySelector('.message__area')
-// do {
-//     name = prompt('Please enter your name: ')
-// } while(!name)
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
 
 textarea.addEventListener('keyup', (e) => {
     if(e.key === 'Enter') {
